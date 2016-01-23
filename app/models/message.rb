93194e594160a8f70658358af3f14d781792a978
takezomiyamoto <takezo.m.1987@gmail.com>
2016-01-23 10:12:51 +0000

    add alert function

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ActiveRecord::Base
+    
+    # Name
+    validates :name , length: {maximum: 20 } , presence: true
+    # Body
+    validates :body , length: {minimum: 2, maximum: 30}, presence: true
+    
 end
